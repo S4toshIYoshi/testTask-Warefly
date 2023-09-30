43edddece8b9d759875a9ab4467585f7f5f9641d
Dery456 <n_krosh@mail.ru>
2023-09-30 12:19:52 +0300

    0f3b4bd

---
+++ assets/default.d1254fb4.js
@@ -0,0 +1,1 @@
+import{_ as t,q as o,M as a,s as e,m as c}from"./entry.9b3069c7.js";const _={},n={class:"layout"},r=e("div",{class:"header"},[e("h2",{class:"title"},"Kroshechkin"),e("h3",{class:"description"},"My projects")],-1);function l(s,d,i,p,u,f){return c(),o("div",n,[r,a(s.$slots,"default")])}const m=t(_,[["render",l]]);export{m as default};

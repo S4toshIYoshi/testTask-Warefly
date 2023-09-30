0f3b4bdeb125789be26a4f3d8b7ca010f87239cf
Dery456 <n_krosh@mail.ru>
2023-09-30 12:18:44 +0300

    fix bug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default {
-	target: 'static',
-	head: {
-		title: process.env.npm_package_name || '',
-		meta: [
-			{ charset: 'utf-8' },
-			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-			{
-				hid: 'description',
-				name: 'description',
-				content: process.env.npm_package_description || '',
-			},
-		],
-		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+
+export default defineNuxtConfig({
+	app: {
+		baseURL: '/testTask-Warefly/',
+		buildAssetsDir: 'assets',
 	},
-	build: {},
-	router: {
-		base: '/testTask-Warefly/',
-	},
-	components: true,
-};
+});
